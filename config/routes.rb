99c5711255f7215d844d1e748ebcb4ed03fe3cca
Ignacio Capuccio <ignacio.capuccio@widergy.com>
2019-10-27 13:45:52 -0300

    Fix routes config (#1)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Sidekiq::Web, at: 'sidekiq'
   # This route catches all requests that does not match with any other previous route declared
-  match '*uri', to: 'errors#routing_error', via: :all
+  match '*a', to: 'errors#routing_error', via: :all
   match '/', to: 'errors#routing_error', via: :all
 end
