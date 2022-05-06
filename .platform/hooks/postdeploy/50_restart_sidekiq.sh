3998e2cb77f963023138478795c993f549931b04
Lucas Enriquez <lucas.enriquez@widergy.com>
2022-05-06 15:46:29 -0300

    [NOTIF-171] Fix scripts deploy

--- .platform/hooks/postdeploy/50_restart_sidekiq.sh
+++ .platform/hooks/postdeploy/50_restart_sidekiq.sh
@@ -1,35 +1,24 @@
-# #!/bin/bash
+#!/bin/bash
 
-# EB_APP_USER=$(/opt/elasticbeanstalk/bin/get-config platformconfig -k AppUser)
-# EB_APP_DEPLOY_DIR="/var/app/current"
-# EB_APP_PID_DIR="/var/pids"
-# EB_SUPPORT_DIR="/opt/elasticbeanstalk/support"
-
-# SIDEKIQ_PID=$EB_APP_PID_DIR/sidekiq.pid
-# SIDEKIQ_CONFIG=$EB_APP_DEPLOY_DIR/config/sidekiq.yml
-# SIDEKIQ_LOG=$EB_APP_DEPLOY_DIR/log/sidekiq.log
-
-# RACK_ENV=$(/opt/elasticbeanstalk/bin/get-config environment -k RACK_ENV)
-
-# cd $EB_APP_DEPLOY_DIR
-
-# if [ -f $SIDEKIQ_PID ]
-# then
-#   echo "terminating existing sidekiq"
-#   su -s /bin/bash -c "kill -TERM `cat $SIDEKIQ_PID`" $EB_APP_USER
-#   su -s /bin/bash -c "rm -rf $SIDEKIQ_PID" $EB_APP_USER
-# fi
-
-# sleep 10
-
-# set -xe
-
-# export $(cat /opt/elasticbeanstalk/deployment/env | xargs)
-
-# echo "starting sidekiq"
-# su -s /bin/bash -c "bundle exec sidekiq \
-#   -e $RACK_ENV \
-#   -P $SIDEKIQ_PID \
-#   -C $SIDEKIQ_CONFIG \
-#   -L $SIDEKIQ_LOG \
-#   -d" $EB_APP_USER
+EB_APP_DEPLOY_DIR=$(/opt/elasticbeanstalk/bin/get-config platformconfig -k AppDeployDir)
+EB_APP_USER=$(/opt/elasticbeanstalk/bin/get-config platformconfig -k AppUser)
+RACK_ENV=$(/opt/elasticbeanstalk/bin/get-config environment -k RACK_ENV)
+SIDEKIQ_PID=$(pgrep -u "$EB_APP_USER" -f sidekiq)
+SIDEKIQ_LOG="$EB_APP_DEPLOY_DIR/log/sidekiq.log"
+ 
+cd "$EB_APP_DEPLOY_DIR"
+ 
+if [ -n "$SIDEKIQ_PID" ]; then
+  echo "terminating existing sidekiq"
+  kill -TERM "$SIDEKIQ_PID"
+  # wait process to get killed
+  while kill -0 "$SIDEKIQ_PID" 2>/dev/null; do
+    sleep 1
+  done
+fi
+ 
+set -xe
+export $(cat /opt/elasticbeanstalk/deployment/env | xargs)
+ 
+echo "starting sidekiq"
+su "$EB_APP_USER" -c "bundle exec sidekiq -e $RACK_ENV > $SIDEKIQ_LOG 2>&1 & disown"
