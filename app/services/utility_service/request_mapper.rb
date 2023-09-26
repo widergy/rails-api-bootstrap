2955582ba0aa6b92e610cbe09ceca4ada7ea66a2
Roy Vicente <roy.vicente@widergy.com>
2023-09-26 13:57:17 -0300

    feat: add annotate

--- app/services/utility_service/request_mapper.rb
+++ app/services/utility_service/request_mapper.rb
@@ -1,6 +1,5 @@
 module UtilityService
   class RequestMapper
-    include PaymentGateways
     def initialize(utility = nil)
       @utility = utility
     end
--- config/routes.rb
+++ config/routes.rb
@@ -24,3 +24,50 @@ end
 
 # == Route Map
 #
+#                          Prefix Verb     URI Pattern                                                                                       Controller#Action
+#                     sidekiq_web          /sidekiq                                                                                          Sidekiq::Web
+#                         pg_hero          /pghero                                                                                           PgHero::Engine
+#                     api_version GET      /api/version(.:format)                                                                            versions#show
+#                                 GET|POST /health_check(/:checks)(.:format)                                                                 health_check#index {:format=>:json}
+#                                          /*a(.:format)                                                                                     errors#routing_error
+#                                          /                                                                                                 errors#routing_error
+#              rails_service_blob GET      /rails/active_storage/blobs/redirect/:signed_id/*filename(.:format)                               active_storage/blobs/redirect#show
+#        rails_service_blob_proxy GET      /rails/active_storage/blobs/proxy/:signed_id/*filename(.:format)                                  active_storage/blobs/proxy#show
+#                                 GET      /rails/active_storage/blobs/:signed_id/*filename(.:format)                                        active_storage/blobs/redirect#show
+#       rails_blob_representation GET      /rails/active_storage/representations/redirect/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations/redirect#show
+# rails_blob_representation_proxy GET      /rails/active_storage/representations/proxy/:signed_blob_id/:variation_key/*filename(.:format)    active_storage/representations/proxy#show
+#                                 GET      /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format)          active_storage/representations/redirect#show
+#              rails_disk_service GET      /rails/active_storage/disk/:encoded_key/*filename(.:format)                                       active_storage/disk#show
+#       update_rails_disk_service PUT      /rails/active_storage/disk/:encoded_token(.:format)                                               active_storage/disk#update
+#            rails_direct_uploads POST     /rails/active_storage/direct_uploads(.:format)                                                    active_storage/direct_uploads#create
+#                   async_request          /async_request                                                                                    AsyncRequest::Engine
+#
+# Routes for PgHero::Engine:
+#                     space GET  (/:database)/space(.:format)                     pg_hero/home#space
+#            relation_space GET  (/:database)/space/:relation(.:format)           pg_hero/home#relation_space
+#               index_bloat GET  (/:database)/index_bloat(.:format)               pg_hero/home#index_bloat
+#              live_queries GET  (/:database)/live_queries(.:format)              pg_hero/home#live_queries
+#                   queries GET  (/:database)/queries(.:format)                   pg_hero/home#queries
+#                show_query GET  (/:database)/queries/:query_hash(.:format)       pg_hero/home#show_query
+#                    system GET  (/:database)/system(.:format)                    pg_hero/home#system
+#                 cpu_usage GET  (/:database)/cpu_usage(.:format)                 pg_hero/home#cpu_usage
+#          connection_stats GET  (/:database)/connection_stats(.:format)          pg_hero/home#connection_stats
+#     replication_lag_stats GET  (/:database)/replication_lag_stats(.:format)     pg_hero/home#replication_lag_stats
+#                load_stats GET  (/:database)/load_stats(.:format)                pg_hero/home#load_stats
+#          free_space_stats GET  (/:database)/free_space_stats(.:format)          pg_hero/home#free_space_stats
+#                   explain GET  (/:database)/explain(.:format)                   pg_hero/home#explain
+#                      tune GET  (/:database)/tune(.:format)                      pg_hero/home#tune
+#               connections GET  (/:database)/connections(.:format)               pg_hero/home#connections
+#               maintenance GET  (/:database)/maintenance(.:format)               pg_hero/home#maintenance
+#                      kill POST (/:database)/kill(.:format)                      pg_hero/home#kill
+# kill_long_running_queries POST (/:database)/kill_long_running_queries(.:format) pg_hero/home#kill_long_running_queries
+#                  kill_all POST (/:database)/kill_all(.:format)                  pg_hero/home#kill_all
+#        enable_query_stats POST (/:database)/enable_query_stats(.:format)        pg_hero/home#enable_query_stats
+#                           POST (/:database)/explain(.:format)                   pg_hero/home#explain
+#         reset_query_stats POST (/:database)/reset_query_stats(.:format)         pg_hero/home#reset_query_stats
+#              system_stats GET  (/:database)/system_stats(.:format)              redirect(301, system)
+#               query_stats GET  (/:database)/query_stats(.:format)               redirect(301, queries)
+#                      root GET  /(:database)(.:format)                           pg_hero/home#index
+#
+# Routes for AsyncRequest::Engine:
+#    job GET  /jobs/:id(.:format) async_request/jobs#show
