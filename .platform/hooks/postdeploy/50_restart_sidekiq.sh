1a17a659387d664e707eaca0f6a9c57f80cdfe0f
Florencia Frioli <28274491+florfrioli@users.noreply.github.com>
2024-08-29 14:06:20 -0300

    fix: [WTP-1668] add autoscaling policy and monit/sidekiq changes (#63)

--- .platform/hooks/postdeploy/50_restart_sidekiq.sh
+++ .platform/hooks/postdeploy/50_restart_sidekiq.sh
@@ -4,21 +4,27 @@ EB_APP_DEPLOY_DIR=$(/opt/elasticbeanstalk/bin/get-config platformconfig -k AppDe
 EB_APP_USER=$(/opt/elasticbeanstalk/bin/get-config platformconfig -k AppUser)
 RACK_ENV=$(/opt/elasticbeanstalk/bin/get-config environment -k RACK_ENV)
 SIDEKIQ_PID=$(pgrep -u "$EB_APP_USER" -f sidekiq)
-SIDEKIQ_LOG="$EB_APP_DEPLOY_DIR/log/sidekiq.log"
- 
-cd "$EB_APP_DEPLOY_DIR"
+SIDEKIQ_LOG="${EB_APP_DEPLOY_DIR}log/sidekiq.log"
+SIDEKIQ_PID_FILE="/var/pids/sidekiq.pid"
+
+cd "$EB_APP_DEPLOY_DIR" || { echo "Error al cambiar al directorio de despliegue"; exit 1; }
  
 if [ -n "$SIDEKIQ_PID" ]; then
-  echo "terminating existing sidekiq"
+  echo "---> Terminating existing sidekiq with PID $SIDEKIQ_PID"
   kill -TERM "$SIDEKIQ_PID"
   # wait process to get killed
   while kill -0 "$SIDEKIQ_PID" 2>/dev/null; do
     sleep 1
   done
+else
+  echo "---> Sidekiq is not running, PID: $SIDEKIQ_PID"
 fi
  
 set -xe
 export $(cat /opt/elasticbeanstalk/deployment/env | xargs)
  
-echo "starting sidekiq"
-su "$EB_APP_USER" -c "bundle exec sidekiq -e $RACK_ENV > $SIDEKIQ_LOG 2>&1 & disown"
+echo "---> Starting sidekiq"
+
+su "$EB_APP_USER" -c "bundle exec sidekiq -e $RACK_ENV >> $SIDEKIQ_LOG 2>&1 & echo \$! > $SIDEKIQ_PID_FILE"
+
+echo "---> Sidekiq started with PID $(cat $SIDEKIQ_PID_FILE)"
--- .platform/hooks/predeploy/03_mute_sidekiq.sh
+++ .platform/hooks/predeploy/03_mute_sidekiq.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-# ref: https://forums.aws.amazon.com/thread.jspa?threadID=330819
  
 EB_APP_USER=$(/opt/elasticbeanstalk/bin/get-config platformconfig -k AppUser)
 SIDEKIQ_PID=$(/usr/bin/pgrep -u "$EB_APP_USER" -f sidekiq)
  
 if [ -n "$SIDEKIQ_PID" ]; then
-  echo "TERM/quieting sidekiq"
+  echo "---> TERM/quieting Sidekiq"
   kill -TERM "$SIDEKIQ_PID"
 fi
